205b6ff9491f02863339a2e77d1c0068ac049f87
bibica <119997727+bibicadotnet@users.noreply.github.com>
2025-02-16 22:04:46 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,28 +1,31 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-
+    
     if (url.hostname === 'i.bibica.net') {
       const wpUrl = new URL(request.url);
       wpUrl.hostname = 'i0.wp.com';
       wpUrl.pathname = '/bibica.net/wp-content/uploads' + url.pathname;
       wpUrl.search = url.search;
+
       try {
         const imageResponse = await fetch(wpUrl, {
           headers: {
-            'Accept': request.headers.get('Accept') || '/'
+            'Accept': request.headers.get('Accept') || '*/*'
           }
         });
-
+        
         if (!imageResponse.ok) {
-          throw new Error(Failed to fetch image: ${imageResponse.status});
+          throw new Error(`Failed to fetch image: ${imageResponse.status}`);
         }
-        const canonicalUrl = http://bibica.net/wp-content/uploads/${url.pathname};
+
+        const canonicalUrl = `http://bibica.net/wp-content/uploads/${url.pathname}`;
+
         return new Response(imageResponse.body, {
           headers: {
             'content-type': imageResponse.headers.get('content-type'),
             'vary': 'Accept',
-            'Link': <${canonicalUrl}>; rel="canonical"
+            'Link': `<${canonicalUrl}>; rel="canonical"`
           }
         });
       } catch (error) {
@@ -30,6 +33,7 @@ export default {
         return fetch(new Request(wpUrl, request));
       }
     }
+
     return new Response('Not Found', { status: 404 });
   }
 };
