f4923108a1e5f8fafe4d9c4c74504deeddbc72d4
bibica <119997727+bibicadotnet@users.noreply.github.com>
2025-02-17 11:59:52 +0700

    Create simply.js

---
+++ simply.js
@@ -0,0 +1,26 @@
+export default {
+  async fetch(request) {
+    const url = new URL(request.url);
+    if (url.hostname === 'i.bibica.net') {
+      const wpUrl = new URL(request.url);
+      wpUrl.hostname = 'i0.wp.com';
+      wpUrl.pathname = '/bibica.net/wp-content/uploads' + url.pathname;
+      wpUrl.search = url.search;
+      const imageResponse = await fetch(wpUrl, {
+        headers: { 'Accept': request.headers.get('Accept') || '/' }
+      });
+      const canonicalUrl = http://bibica.net/wp-content/uploads${url.pathname};
+      return new Response(imageResponse.body, {
+        headers: {
+          'content-type': imageResponse.headers.get('content-type'),
+          'vary': 'Accept',
+          'Link': <${canonicalUrl}>; rel="canonical",
+          'Cache-Control': 'public, max-age=31536000, immutable',',
+          'CDN-Cache-Control': 'public, max-age=31536000, immutable',',
+          'Cloudflare-CDN-Cache-Control': 'public, max-age=31536000, immutable',',
+        }
+      });
+    }
+    return new Response(Request not supported: ${url.hostname} does not match any rules., { status: 404 });
+  }
+};
