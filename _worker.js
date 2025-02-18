aca202b10b54b0bdebe82f450037fcce43a88525
r2-bibica-net <r2@bibica.net>
2025-02-19 01:14:04 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,32 +2,30 @@ export default {
   async fetch(request) {
     const url = new URL(request.url);
     if (url.hostname === 'i.bibica.net') {
+      // Normalize URL
       const wpUrl = new URL(request.url);
       wpUrl.hostname = 'i0.wp.com';
       wpUrl.pathname = '/bibica.net/wp-content/uploads' + url.pathname;
       wpUrl.search = url.search;
 
-      // Tạo cache key cố định
-      const customCacheKey = new Request(url.pathname + url.search, {
-        method: 'GET'
-      });
-      
+      // Tạo request mới với chỉ Accept header
       const wpRequest = new Request(wpUrl.toString(), {
         method: 'GET',
+        duplex: 'half',
         headers: {
           'Accept': 'image/webp'
         }
       });
 
-      // Sử dụng cacheKey tùy chỉnh thay vì request gốc
-      const response = await fetch(wpRequest, {
-        cf: {
-          // Cache dựa trên customCacheKey thay vì request
-          cacheKey: customCacheKey
+      const imageResponse = await fetch(wpRequest);
+
+      // Return response với minimal headers
+      return new Response(imageResponse.body, {
+        headers: {
+          'content-type': 'image/webp',
+          'Cache-Control': 'public, max-age=31536000'
         }
       });
-
-      return response;
     }
     return new Response(`Request not supported`, { status: 404 });
   }
