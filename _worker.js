ec8950ab208725f2cd89603dc60843bbc4918858
r2-bibica-net <r2@bibica.net>
2025-02-18 19:34:45 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,17 +1,12 @@
 export default {
   async fetch(request) {
     const url = new URL(request.url);
-
     if (url.hostname === 'i.bibica.net') {
       const wpUrl = new URL(request.url);
       wpUrl.hostname = 'i0.wp.com';
       wpUrl.pathname = '/bibica.net/wp-content/uploads' + url.pathname;
       wpUrl.search = url.search;
-
-      const imageResponse = await fetch(wpUrl, {
-        headers: { 'Accept': request.headers.get('Accept') || '*/*' }
-      });
-
+      const imageResponse = await fetch(wpUrl); // Loại bỏ headers không cần thiết
       return new Response(imageResponse.body, {
         headers: {
           'link': imageResponse.headers.get('link'),
