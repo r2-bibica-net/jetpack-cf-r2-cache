2da94ca7bb6ac0a8d61f96f509ba7a8441ac3a1c
r2-bibica-net <r2@bibica.net>
2025-02-18 21:46:22 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,16 +1,25 @@
 export default {
   async fetch(request) {
     const url = new URL(request.url);
+
     if (url.hostname === 'i.bibica.net') {
       const wpUrl = new URL(request.url);
       wpUrl.hostname = 'i0.wp.com';
       wpUrl.pathname = '/bibica.net/wp-content/uploads' + url.pathname;
       wpUrl.search = url.search;
-      const imageResponse = await fetch(wpUrl); // Loại bỏ headers không cần thiết
+
+      const imageResponse = await fetch(wpUrl, {
+        headers: { 'Accept': request.headers.get('Accept') || '*/*' }
+      });
+      const canonicalUrl = `http://bibica.net/wp-content/uploads${url.pathname}`;
+
       return new Response(imageResponse.body, {
         headers: {
-          'link': imageResponse.headers.get('link'),
-          'x-nc': imageResponse.headers.get('x-nc'),
+          'content-type': imageResponse.headers.get('content-type'),
+          'vary': 'Accept',
+          'Link': `<${canonicalUrl}>; rel="canonical"`,
+          'Cache-Control': 'public, max-age=31536000, immutable, no-transform',
+          'Pragma': 'public',
           'X-Served-By': 'Cloudflare & Jetpack'
         }
       });
