37a5eac61f8e2a1a8a256e71c844162dc75926aa
r2-bibica-net <r2@bibica.net>
2025-02-18 18:27:17 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -8,21 +8,7 @@ export default {
       wpUrl.pathname = '/bibica.net/wp-content/uploads' + url.pathname;
       wpUrl.search = url.search;
 
-      const imageResponse = await fetch(wpUrl, {
-        headers: { 'Accept': request.headers.get('Accept') || '*/*' }
-      });
-      const canonicalUrl = `http://bibica.net/wp-content/uploads${url.pathname}`;
-
       return new Response(imageResponse.body, {
-        headers: {
-          'cache-control': imageResponse.headers.get('cache-control'),
-          'expires': imageResponse.headers.get('expires'),
-          'last-modified': imageResponse.headers.get('last-modified'),
-          'link': imageResponse.headers.get('link'),
-          'etag': imageResponse.headers.get('etag'),
-          'x-nc': imageResponse.headers.get('x-nc'),
-          'X-Served-By': 'Cloudflare & Jetpack'
-        }
       });
     }
     return new Response(`Request not supported: ${url.hostname} does not match any rules.`, { status: 404 });
