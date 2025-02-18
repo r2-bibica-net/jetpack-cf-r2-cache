9975b9ad871ebdf9dafdc510308410cb32bff18d
r2-bibica-net <r2@bibica.net>
2025-02-18 18:12:03 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -16,6 +16,7 @@ export default {
       return new Response(imageResponse.body, {
         headers: {
           'content-type': imageResponse.headers.get('content-type'),
+          'last-modified': imageResponse.headers.get('last-modified'),
           'vary': 'Accept',
           'Link': `<${canonicalUrl}>; rel="canonical"`,
           'Cache-Control': 'public, max-age=31536000, immutable, no-transform',
