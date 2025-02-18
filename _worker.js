24a70830b049ffa73a61b2042f9fa8f94aa24dc2
r2-bibica-net <r2@bibica.net>
2025-02-18 17:26:57 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,55 +2,28 @@ export default {
   async fetch(request) {
     const url = new URL(request.url);
 
-    // Kiểm tra nếu hostname là 'i.bibica.net'
     if (url.hostname === 'i.bibica.net') {
       const wpUrl = new URL(request.url);
-
-      // Thay đổi hostname thành 'i0.wp.com'
       wpUrl.hostname = 'i0.wp.com';
-
-      // Thay đổi pathname để trỏ đến thư mục uploads trên WordPress
       wpUrl.pathname = '/bibica.net/wp-content/uploads' + url.pathname;
-
-      // Giữ nguyên query string (nếu có)
       wpUrl.search = url.search;
 
-      // Gửi yêu cầu đến server mới (i0.wp.com)
       const imageResponse = await fetch(wpUrl, {
-        headers: {
-          'Accept': request.headers.get('Accept') || '/',
-          'If-None-Match': request.headers.get('If-None-Match') || '', // Chuyển tiếp ETag nếu có
-          'If-Modified-Since': request.headers.get('If-Modified-Since') || '' // Chuyển tiếp Last-Modified nếu có
-        }
+        headers: { 'Accept': request.headers.get('Accept') || '*/*' }
       });
+      const canonicalUrl = `http://bibica.net/wp-content/uploads${url.pathname}`;
 
-      // Nếu phản hồi là 304 Not Modified, trả về phản hồi tương ứng
-      if (imageResponse.status === 304) {
-        return new Response(null, {
-          status: 304,
-          headers: {
-            'Cache-Control': 'public, max-age=63115200', // Cache trong 2 năm
-            'Expires': 'Tue, 16 Feb 2027 22:35:06 GMT',
-            'ETag': imageResponse.headers.get('ETag') || '',
-            'Last-Modified': imageResponse.headers.get('Last-Modified') || ''
-          }
-        });
-      }
-
-      // Trả về phản hồi từ server mới với các headers cache phù hợp
       return new Response(imageResponse.body, {
         headers: {
-          'Content-Type': imageResponse.headers.get('Content-Type') || 'image/png',
-          'Cache-Control': 'public, max-age=63115200', // Cache trong 2 năm
-          'Expires': 'Tue, 16 Feb 2027 22:35:06 GMT',
-          'ETag': imageResponse.headers.get('ETag') || '',
-          'Last-Modified': imageResponse.headers.get('Last-Modified') || '',
-          'Vary': 'Accept' // Giữ nguyên header Vary
+          'content-type': imageResponse.headers.get('content-type'),
+          'vary': 'Accept',
+          'Link': `<${canonicalUrl}>; rel="canonical"`,
+          'Cache-Control': 'public, max-age=31536000, immutable, no-transform',
+          'Pragma': 'public',
+          'X-Served-By': 'Cloudflare & Jetpack'
         }
       });
     }
-
-    // Nếu hostname không phải 'i.bibica.net', trả về lỗi 404
     return new Response(`Request not supported: ${url.hostname} does not match any rules.`, { status: 404 });
   }
 };
