01313abb50a84b3bea56a9b9e4492c30a869b4e9
r2-bibica-net <r2@bibica.net>
2025-02-20 09:51:50 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,22 +2,35 @@ export default {
   async fetch(request) {
     const url = new URL(request.url);
 
+    // Bảng ánh xạ đường dẫn
+    const pathMappings = [
+      { prefix: '/comment/avatar', target: 'https://gravatar.webp.se' },
+      { prefix: '/comment', target: 'https://comment.bibica.net/static/images' },
+      { prefix: '/', target: 'https://i0.wp.com/bibica.net/wp-content/uploads' }
+    ];
+
     if (url.hostname === 'i.bibica.net') {
-      const wpUrl = `https://i0.wp.com/bibica.net/wp-content/uploads${url.pathname}${url.search}`;
+      // Tìm URL mapping phù hợp
+      const mapping = pathMappings.find(m => url.pathname.startsWith(m.prefix));
 
-      const imageResponse = await fetch(wpUrl, {
-        headers: { 'Accept': request.headers.get('Accept') || '*/*' }
-      });
+      if (mapping) {
+        const targetUrl = `${mapping.target}${url.pathname.replace(m.prefix, '')}${url.search}`;
 
-      return new Response(imageResponse.body, {
-        headers: {
-          'content-type': 'image/webp',
-          'link': imageResponse.headers.get('link'),
-          'X-Cache': imageResponse.headers.get('x-nc'),
-          'X-Served-By': 'Cloudflare Pages & Jetpack'
-        }
-      });
+        const imageResponse = await fetch(targetUrl, {
+          headers: { 'Accept': request.headers.get('Accept') || '*/*' }
+        });
+
+        return new Response(imageResponse.body, {
+          headers: {
+            'content-type': 'image/webp',
+            'link': imageResponse.headers.get('link'),
+            'X-Cache': imageResponse.headers.get('x-nc'),
+            'X-Served-By': 'Cloudflare Pages & Jetpack'
+          }
+        });
+      }
     }
+
     return new Response(`Request not supported: ${url.hostname} does not match any rules.`, { status: 404 });
   }
 };
