00903b18d3221e2ed00ae86aabb1a11f357a275a
r2-bibica-net <r2@bibica.net>
2025-02-22 05:08:25 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,12 +6,8 @@ export default {
       return new Response('Not found', { status: 404 });
     }
 
-    // Set Accept header ngay từ đầu
-    const headers = {
-      'Accept': 'image/webp,*/*'
-    };
-
     let targetUrl;
+
     if (url.pathname.startsWith('/avatar')) {
       targetUrl = `https://secure.gravatar.com/avatar${url.pathname.slice(7)}`;
     } else if (url.pathname.startsWith('/comment')) {
@@ -20,6 +16,6 @@ export default {
       targetUrl = `https://i0.wp.com/bibica.net/wp-content/uploads${url.pathname}${url.search}`;
     }
 
-    return fetch(targetUrl, { headers });
+    return fetch(targetUrl);
   }
 };
