1d387d723ed1d482dd96cb7f0d485e6bb5b8b13b
r2-bibica-net <r2@bibica.net>
2025-02-21 10:30:05 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,61 +3,69 @@ export default {
     const url = new URL(request.url);
     
     if (url.hostname === 'i.bibica.net') {
-      // Chỉ giữ lại những headers thực sự cần thiết
-      const normalizedHeaders = {
-        'Accept': 'image/webp,*/*'
-      };
-
+      // Chuẩn hóa Accept header cho mọi request
+      const standardAccept = 'image/webp';
+      
       if (url.pathname.startsWith('/avatar')) {
         const gravatarUrl = new URL(request.url);
         gravatarUrl.hostname = 'secure.gravatar.com';
         gravatarUrl.pathname = '/avatar' + url.pathname.replace('/avatar', '');
         
         const gravatarResponse = await fetch(gravatarUrl, {
-          headers: normalizedHeaders
+          headers: { 'Accept': standardAccept }
         });
 
         return new Response(gravatarResponse.body, {
           headers: {
             'content-type': 'image/webp',
-            'Cache-Control': 'public, s-maxage=86400',
+            'Cache-Control': 'public, max-age=31536000',
             'X-Cache': gravatarResponse.headers.get('x-nc'),
             'X-Served-By': 'Cloudflare Pages & Gravatar'
           }
         });
-
       } else if (url.pathname.startsWith('/comment')) {
         const commentUrl = new URL(request.url);
         commentUrl.hostname = 'i0.wp.com';
         commentUrl.pathname = '/comment.bibica.net/static/images' + url.pathname.replace('/comment', '');
         
+        // Xóa query string không cần thiết
+        commentUrl.search = '';
+        
         const commentResponse = await fetch(commentUrl, {
-          headers: normalizedHeaders
+          headers: { 'Accept': standardAccept }
         });
 
         return new Response(commentResponse.body, {
           headers: {
             'content-type': 'image/webp',
-            'Cache-Control': 'public, s-maxage=86400',
+            'Cache-Control': 'public, max-age=31536000',
             'X-Cache': commentResponse.headers.get('x-nc'),
             'X-Served-By': 'Cloudflare Pages & Artalk & Jetpack'
           }
         });
-
       } else {
         const wpUrl = new URL(request.url);
         wpUrl.hostname = 'i0.wp.com';
         wpUrl.pathname = '/bibica.net/wp-content/uploads' + url.pathname;
-        wpUrl.search = url.search;
         
+        // Giữ lại các query string cần thiết cho wp.com
+        const validParams = ['w', 'h', 'quality', 'strip'];
+        const params = new URLSearchParams();
+        for (const [key, value] of new URLSearchParams(url.search)) {
+          if (validParams.includes(key)) {
+            params.append(key, value);
+          }
+        }
+        wpUrl.search = params.toString();
+
         const imageResponse = await fetch(wpUrl, {
-          headers: normalizedHeaders
+          headers: { 'Accept': standardAccept }
         });
 
         return new Response(imageResponse.body, {
           headers: {
             'content-type': 'image/webp',
-            'Cache-Control': 'public, s-maxage=86400',
+            'Cache-Control': 'public, max-age=31536000',
             'X-Cache': imageResponse.headers.get('x-nc'),
             'X-Served-By': 'Cloudflare Pages & Jetpack'
           }
@@ -65,6 +73,6 @@ export default {
       }
     }
 
-    return new Response(`Request not supported: ${url.hostname} does not match any rules.`, { status: 404 });
+    return new Response(`Request not supported: ${url.hostname}`, { status: 404 });
   }
 };
