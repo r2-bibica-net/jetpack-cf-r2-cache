644078f83acdf46d136d233e1ae498e3de4a778f
r2-bibica-net <r2@bibica.net>
2025-02-22 02:27:46 +0700

    Create test01.js

---
+++ test01.js
@@ -0,0 +1,21 @@
+export default {
+  async fetch(request) {
+    const url = new URL(request.url);
+    
+    if (url.hostname !== 'i.bibica.net') {
+      return new Response('Not found', { status: 404 });
+    }
+
+    let targetUrl;
+
+    if (url.pathname.startsWith('/avatar')) {
+      targetUrl = `https://secure.gravatar.com/avatar${url.pathname.slice(7)}`;
+    } else if (url.pathname.startsWith('/comment')) {
+      targetUrl = `https://i0.wp.com/comment.bibica.net/static/images${url.pathname.slice(8)}`;
+    } else {
+      targetUrl = `https://i0.wp.com/bibica.net/wp-content/uploads${url.pathname}${url.search}`;
+    }
+
+    return fetch(targetUrl);
+  }
+};
