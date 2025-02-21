45d41e938e36c382f96d53135162ad26f4adebea
r2-bibica-net <r2@bibica.net>
2025-02-22 02:08:31 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,28 +7,15 @@ export default {
     }
 
     let targetUrl;
-    let source;
 
     if (url.pathname.startsWith('/avatar')) {
       targetUrl = `https://secure.gravatar.com/avatar${url.pathname.slice(7)}`;
-      source = 'Gravatar';
     } else if (url.pathname.startsWith('/comment')) {
       targetUrl = `https://i0.wp.com/comment.bibica.net/static/images${url.pathname.slice(8)}`;
-      source = 'Artalk & Jetpack';
     } else {
       targetUrl = `https://i0.wp.com/bibica.net/wp-content/uploads${url.pathname}${url.search}`;
-      source = 'Jetpack';
     }
 
-    const response = await fetch(targetUrl);
-    
-    // Override Vary header để tránh tạo nhiều cache variants
-    const headers = new Headers(response.headers);
-    headers.set('Vary', '');
-    headers.set('Cache-Control', 'public, max-age=31536000');
-    
-    return new Response(response.body, {
-      headers: headers
-    });
+    return fetch(targetUrl);
   }
 };
