8670db972faad4ac713abb53cecc14f9c471e51e
r2-bibica-net <r2@bibica.net>
2025-02-22 01:21:20 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,42 +1,117 @@
 export default {
+
   async fetch(request) {
+
     const url = new URL(request.url);
-    
-    if (url.hostname !== 'i.bibica.net') {
-      return new Response(`Request not supported`, { status: 404 });
+
+    if (url.hostname === 'i.bibica.net') {
+
+      if (url.pathname.startsWith('/avatar')) {
+
+        // Chuyển hướng i.bibica.net/comment/avatar sang https://secure.gravatar.com/avatar
+
+        const gravatarUrl = new URL(request.url);
+
+        gravatarUrl.hostname = 'secure.gravatar.com';
+
+        gravatarUrl.pathname = '/avatar' + url.pathname.replace('/avatar', '');
+
+        const gravatarResponse = await fetch(gravatarUrl, {
+
+          headers: { 'Accept': request.headers.get('Accept') || '*/*' }
+
+        });
+
+        return new Response(gravatarResponse.body, {
+
+          headers: {
+
+            'content-type': 'image/webp',
+
+            'link': gravatarResponse.headers.get('link'),
+
+            'X-Cache': gravatarResponse.headers.get('x-nc'),
+
+            'X-Served-By': 'Cloudflare Pages & Gravatar'
+
+          }
+
+        });
+
+      } else if (url.pathname.startsWith('/comment')) {
+
+        // Chuyển hướng i.bibica.net/comment sang https://i0.wp.com/comment.bibica.net/static/images
+
+        const commentUrl = new URL(request.url);
+
+        commentUrl.hostname = 'i0.wp.com';
+
+        commentUrl.pathname = '/comment.bibica.net/static/images' + url.pathname.replace('/comment', '');
+
+        const commentResponse = await fetch(commentUrl, {
+
+          headers: { 'Accept': request.headers.get('Accept') || '*/*' }
+
+        });
+
+        return new Response(commentResponse.body, {
+
+          headers: {
+
+            'content-type': 'image/webp',
+
+            'link': commentResponse.headers.get('link'),
+
+            'X-Cache': commentResponse.headers.get('x-nc'),
+
+            'X-Served-By': 'Cloudflare Pages & Artalk & Jetpack'
+
+          }
+
+        });
+
+      } else {
+
+        // Chuyển hướng mặc định cho i.bibica.net
+
+        const wpUrl = new URL(request.url);
+
+        wpUrl.hostname = 'i0.wp.com';
+
+        wpUrl.pathname = '/bibica.net/wp-content/uploads' + url.pathname;
+
+        wpUrl.search = url.search;
+
+        const imageResponse = await fetch(wpUrl, {
+
+          headers: { 'Accept': request.headers.get('Accept') || '*/*' }
+
+        });
+
+        return new Response(imageResponse.body, {
+
+          headers: {
+
+            'content-type': 'image/webp',
+
+            'link': imageResponse.headers.get('link'),
+
+            'X-Cache': imageResponse.headers.get('x-nc'),
+
+            'X-Served-By': 'Cloudflare Pages & Jetpack'
+
+          }
+
+        });
+
+      }
+
     }
 
-    let targetUrl = new URL(request.url);
-    let source = '';
+    // Trả về lỗi 404 nếu không khớp với bất kỳ quy tắc nào
 
-    // Xử lý routing
-    if (url.pathname.startsWith('/avatar')) {
-      targetUrl.hostname = 'secure.gravatar.com';
-      targetUrl.pathname = '/avatar' + url.pathname.replace('/avatar', '');
-      source = 'Gravatar';
-    } else if (url.pathname.startsWith('/comment')) {
-      targetUrl.hostname = 'i0.wp.com';
-      targetUrl.pathname = '/comment.bibica.net/static/images' + url.pathname.replace('/comment', '');
-      source = 'Artalk & Jetpack';
-    } else {
-      targetUrl.hostname = 'i0.wp.com'; 
-      targetUrl.pathname = '/bibica.net/wp-content/uploads' + url.pathname;
-      targetUrl.search = url.search;
-      source = 'Jetpack';
-    }
+    return new Response(`Request not supported: ${url.hostname} does not match any rules.`, { status: 404 });
 
-    try {
-      const sourceResponse = await fetch(targetUrl.toString());
-
-      return new Response(sourceResponse.body, {
-        headers: {
-          'Content-Type': 'image/webp',
-          'Cache-Control': 'public, max-age=31536000',
-          'X-Served-By': `Cloudflare Pages & ${source}`
-        }
-      });
-    } catch (error) {
-      return new Response('Error fetching image', { status: 500 });
-    }
   }
+
 };
