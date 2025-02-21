22d481891444d5511ef611da7b043a28402c8f88
r2-bibica-net <r2@bibica.net>
2025-02-21 10:10:44 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,73 +1,59 @@
 export default {
   async fetch(request) {
     const url = new URL(request.url);
-    
     if (url.hostname === 'i.bibica.net') {
-        // Lấy tất cả các headers từ request gốc để xem có gì
-      const originalHeaders = {};
-      for (const [key, value] of request.headers) {
-        console.log(`${key}: ${value}`);
-        originalHeaders[key] = value;
-      }
-      };
-
       if (url.pathname.startsWith('/avatar')) {
+        // Chuyển hướng i.bibica.net/comment/avatar sang https://secure.gravatar.com/avatar
         const gravatarUrl = new URL(request.url);
         gravatarUrl.hostname = 'secure.gravatar.com';
         gravatarUrl.pathname = '/avatar' + url.pathname.replace('/avatar', '');
-        
         const gravatarResponse = await fetch(gravatarUrl, {
-          headers: normalizedHeaders
+          headers: { 'Accept': request.headers.get('Accept') || '/' }
         });
-
         return new Response(gravatarResponse.body, {
           headers: {
             'content-type': 'image/webp',
-            'Cache-Control': 'public, s-maxage=86400',
+            'link': gravatarResponse.headers.get('link'),
             'X-Cache': gravatarResponse.headers.get('x-nc'),
             'X-Served-By': 'Cloudflare Pages & Gravatar'
           }
         });
-
       } else if (url.pathname.startsWith('/comment')) {
+        // Chuyển hướng i.bibica.net/comment sang https://i0.wp.com/comment.bibica.net/static/images
         const commentUrl = new URL(request.url);
         commentUrl.hostname = 'i0.wp.com';
         commentUrl.pathname = '/comment.bibica.net/static/images' + url.pathname.replace('/comment', '');
-        
         const commentResponse = await fetch(commentUrl, {
-          headers: normalizedHeaders
+          headers: { 'Accept': request.headers.get('Accept') || '/' }
         });
-
         return new Response(commentResponse.body, {
           headers: {
             'content-type': 'image/webp',
-            'Cache-Control': 'public, s-maxage=86400',
+            'link': commentResponse.headers.get('link'),
             'X-Cache': commentResponse.headers.get('x-nc'),
             'X-Served-By': 'Cloudflare Pages & Artalk & Jetpack'
           }
         });
-
       } else {
+        // Chuyển hướng mặc định cho i.bibica.net
         const wpUrl = new URL(request.url);
         wpUrl.hostname = 'i0.wp.com';
         wpUrl.pathname = '/bibica.net/wp-content/uploads' + url.pathname;
         wpUrl.search = url.search;
-        
         const imageResponse = await fetch(wpUrl, {
-          headers: normalizedHeaders
+          headers: { 'Accept': request.headers.get('Accept') || '/' }
         });
-
         return new Response(imageResponse.body, {
           headers: {
             'content-type': 'image/webp',
-            'Cache-Control': 'public, s-maxage=86400',
+            'link': imageResponse.headers.get('link'),
             'X-Cache': imageResponse.headers.get('x-nc'),
             'X-Served-By': 'Cloudflare Pages & Jetpack'
           }
         });
       }
     }
-
-    return new Response(`Request not supported: ${url.hostname} does not match any rules.`, { status: 404 });
+    // Trả về lỗi 404 nếu không khớp với bất kỳ quy tắc nào
+    return new Response(Request not supported: ${url.hostname} does not match any rules., { status: 404 });
   }
 };
