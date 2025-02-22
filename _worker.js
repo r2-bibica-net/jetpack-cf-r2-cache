842fbbf1b654ffc4377acf44a638df289c096aa1
r2-bibica-net <r2@bibica.net>
2025-02-22 20:02:58 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,37 +1,67 @@
-export function onRequest(context) {
-  const url = new URL(context.request.url);
-  let targetUrl = new URL(context.request.url);
+export default {
+  async fetch(request) {
+    const url = new URL(request.url);
 
-  if (url.pathname.startsWith('/avatar')) {
-    targetUrl.hostname = 'secure.gravatar.com';
-    targetUrl.pathname = '/avatar' + url.pathname.slice(7);
-    return proxyImage(context.request, targetUrl, 'Cloudflare Pages & Gravatar');
-  }
-  
-  if (url.pathname.startsWith('/comment')) {
-    targetUrl.hostname = 'i0.wp.com';
-    targetUrl.pathname = '/comment.bibica.net/static/images' + url.pathname.slice(8);
-    return proxyImage(context.request, targetUrl, 'Cloudflare Pages & Artalk & Jetpack');
-  }
+    if (url.hostname === 'i.bibica.net') {
+      if (url.pathname.startsWith('/avatar')) {
+        // Chuyển hướng i.bibica.net/comment/avatar sang https://secure.gravatar.com/avatar
+        const gravatarUrl = new URL(request.url);
+        gravatarUrl.hostname = 'secure.gravatar.com';
+        gravatarUrl.pathname = '/avatar' + url.pathname.replace('/avatar', '');
 
-  // Default case
-  targetUrl.hostname = 'i0.wp.com';
-  targetUrl.pathname = '/bibica.net/wp-content/uploads' + url.pathname;
-  targetUrl.search = url.search;
-  return proxyImage(context.request, targetUrl, 'Cloudflare Pages & Jetpack');
-}
+        const gravatarResponse = await fetch(gravatarUrl, {
+          headers: { 'Accept': request.headers.get('Accept') || '*/*' }
+        });
 
-async function proxyImage(request, targetUrl, provider) {
-  const response = await fetch(targetUrl, {
-    headers: { 'Accept': request.headers.get('Accept') || '*/*' }
-  });
+        return new Response(gravatarResponse.body, {
+          headers: {
+            'content-type': 'image/webp',
+            'link': gravatarResponse.headers.get('link'),
+            'X-Cache': gravatarResponse.headers.get('x-nc'),
+            'X-Served-By': 'Cloudflare Pages & Gravatar'
+          }
+        });
+      } else if (url.pathname.startsWith('/comment')) {
+        // Chuyển hướng i.bibica.net/comment sang https://i0.wp.com/comment.bibica.net/static/images
+        const commentUrl = new URL(request.url);
+        commentUrl.hostname = 'i0.wp.com';
+        commentUrl.pathname = '/comment.bibica.net/static/images' + url.pathname.replace('/comment', '');
 
-  return new Response(response.body, {
-    headers: {
-      'content-type': 'image/webp',
-      'link': response.headers.get('link'),
-      'X-Cache': response.headers.get('x-nc'),
-      'X-Served-By': provider
+        const commentResponse = await fetch(commentUrl, {
+          headers: { 'Accept': request.headers.get('Accept') || '*/*' }
+        });
+
+        return new Response(commentResponse.body, {
+          headers: {
+            'content-type': 'image/webp',
+            'link': commentResponse.headers.get('link'),
+            'X-Cache': commentResponse.headers.get('x-nc'),
+            'X-Served-By': 'Cloudflare Pages & Artalk & Jetpack'
+          }
+        });
+      } else {
+        // Chuyển hướng mặc định cho i.bibica.net
+        const wpUrl = new URL(request.url);
+        wpUrl.hostname = 'i0.wp.com';
+        wpUrl.pathname = '/bibica.net/wp-content/uploads' + url.pathname;
+        wpUrl.search = url.search;
+
+        const imageResponse = await fetch(wpUrl, {
+          headers: { 'Accept': request.headers.get('Accept') || '*/*' }
+        });
+
+        return new Response(imageResponse.body, {
+          headers: {
+            'content-type': 'image/webp',
+            'link': imageResponse.headers.get('link'),
+            'X-Cache': imageResponse.headers.get('x-nc'),
+            'X-Served-By': 'Cloudflare Pages & Jetpack'
+          }
+        });
+      }
     }
-  });
-}
+
+    // Trả về lỗi 404 nếu không khớp với bất kỳ quy tắc nào
+    return new Response(`Request not supported: ${url.hostname} does not match any rules.`, { status: 404 });
+  }
+};
