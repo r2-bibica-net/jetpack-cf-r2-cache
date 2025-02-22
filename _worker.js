ecd38a99ef2ff83cd9249dcddc8402b317540f8d
r2-bibica-net <r2@bibica.net>
2025-02-22 19:35:31 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request) {
     const url = new URL(request.url);
     const headers = {
-      'Accept': 'image/webp,*/*'
+      'Accept': 'image/webp'
     };
     
     let targetUrl;
